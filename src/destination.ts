0a8f3f79c5b9d23c38517af6e2d161bd7ee9f1e5
Baptiste Pillot <baptiste@pillot.fr>
2025-02-07 11:42:28 +0100

    Simplified resolveDestination() as routes should always be valid

--- src/destination.ts
+++ src/destination.ts
@@ -1,6 +1,5 @@
-import appDir                  from '@itrocks/app-dir'
-import { isAnyFunctionOrType } from '@itrocks/class-type'
-import { Type }                from '@itrocks/class-type'
+import appDir   from '@itrocks/app-dir'
+import { Type } from '@itrocks/class-type'
 
 export type Destination = string
 
@@ -11,14 +10,6 @@ export function isDestination(destination: any): destination is Destination
 
 export function resolveDestination(destination: Destination): Function | Type | undefined
 {
-	try {
-		const module  = require((destination[0] === '/') ? (appDir + destination) : destination)
-		const feature = module.default ?? Object.values(module)[0]
-		return isAnyFunctionOrType(feature)
-			? feature
-			: undefined
-	}
-	catch {
-		return undefined
-	}
+	const module = require((destination[0] === '/') ? (appDir + destination) : destination)
+	return module.default ?? Object.values(module)[0]
 }
